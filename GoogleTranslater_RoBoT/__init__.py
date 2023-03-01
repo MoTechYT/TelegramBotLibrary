17f8e7f036dcdcb19bd8ffe088167cbff21d9f92
Mo-Tech <72255597+MoTechYT@users.noreply.github.com>
2023-03-01 16:06:11 +0530

    Delete GoogleTranslater_RoBoT directory

--- GoogleTranslater_RoBoT/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Free Google Translate API for Python. Translates totally free of charge."""
-
-__all__ = 'Translator',
-__version__ = '3.1.0-alpha'
-
-
-from GoogleTranslater_RoBoT.client import GoogleTranslater_RoBoT
-from GoogleTranslater_RoBoT.constants import LANGCODES, LANGUAGES  # noqa
