41023da8b70ba8fd812a93777778206f2e0af705
Mo-Tech <72255597+MoTechYT@users.noreply.github.com>
2023-03-01 16:02:25 +0530

    Update __init__.py

--- GoogleTranslater_RoBoT/__init__.py
+++ GoogleTranslater_RoBoT/__init__.py
@@ -1,7 +1,8 @@
 """Free Google Translate API for Python. Translates totally free of charge."""
+
 __all__ = 'Translator',
 __version__ = '3.1.0-alpha'
 
 
-from googletrans.client import Translator
-from googletrans.constants import LANGCODES, LANGUAGES  # noqa
+from GoogleTranslater_RoBoT.client import GoogleTranslater_RoBoT
+from GoogleTranslater_RoBoT.constants import LANGCODES, LANGUAGES  # noqa
